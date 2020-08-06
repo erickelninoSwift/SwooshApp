da422990f1bcacd4c3dcc620a75076dc6b118193
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-08-06 06:01:08 +0200

    swooshapp working with viewcontrollers

---
+++ SWOOSHAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWOOSHAPP
+//
+//  Created by El nino Cholo on 2020/08/06.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
