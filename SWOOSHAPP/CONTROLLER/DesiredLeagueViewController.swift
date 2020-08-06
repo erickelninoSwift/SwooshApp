9ec2c6b2eee27efea984efb8b87bd2d0e00285e5
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-08-07 00:29:55 +0200

    Finished with the swoosh interface

---
+++ SWOOSHAPP/CONTROLLER/DesiredLeagueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DesiredLeagueViewController.swift
+//  SWOOSHAPP
+//
+//  Created by El nino Cholo on 2020/08/06.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class DesiredLeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
