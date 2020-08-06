e95c8c506417ebebaf81672f825f1a92f1fb5203
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-08-06 16:49:40 +0200

    adding the design patterns MVC

---
+++ SWOOSHAPP/VIEWS/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  SWOOSHAPP
+//
+//  Created by El nino Cholo on 2020/08/06.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton
+    
+{
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
